988c752c54d6e50bd4dad233793fa1872fdd7625
3v4Si0N <hector.de.armas.hda@gmail.com>
2020-04-28 18:44:15 +0200

    autogeneration of SSL certificate

--- server-multisession.py
+++ server-multisession.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import base64
 from termcolor import colored
@@ -8,19 +7,12 @@ import time
 import readline
 import ssl
 import argparse
-from pynput.keyboard import Key, Controller
-import signal
 import json
+from datetime import datetime, date
+from OpenSSL import crypto, SSL
+from os import path
 
 readline.parse_and_bind("tab: complete")
-
-"""
-    For HTTPS Server
-        Create certificate using the commands:
-                - openssl genrsa -out private.pem 2048
-                - openssl req -new -x509 -key private.pem -out cacert.pem -days 9999
-"""
-
 CLIENT_DICT = {}
 CURRENT_CLIENT = ""
 KEY_PULSED = False
@@ -216,6 +208,47 @@ class Functions():
         except:
             print (colored("\r\n[!] Error: Writing file!", "red"))
 
+
+class Certificate():
+    def checkCertificateExpiration(self):
+        expired = False
+
+        cert = crypto.load_certificate(crypto.FILETYPE_PEM, open('certificate/cacert.pem', 'rt').read())
+        cert_date = datetime.strptime(cert.get_notAfter().decode('utf-8'),"%Y%m%d%H%M%SZ")
+        today = date.today()
+        current_date = today.strftime("%Y-%m-%d")
+
+        if str(current_date) == str(cert_date).split(" ")[0]:
+            expired = True
+        return expired
+
+    def genCertificate(self, KEY_FILE="certificate/private.pem", CERT_FILE="certificate/cacert.pem"):
+        k = crypto.PKey()
+        k.generate_key(crypto.TYPE_RSA, 4096)
+
+        cert = crypto.X509()
+        cert.get_subject().C = "UK"
+        cert.get_subject().ST = "London"
+        cert.get_subject().L = "London"
+        cert.get_subject().O = "Development"
+        cert.get_subject().CN = "www.google.com"
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(31557600)
+        cert.set_issuer(cert.get_subject())
+        cert.set_pubkey(k)
+        cert.sign(k, 'sha512')
+        with open(CERT_FILE, "wt") as f:
+            f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8"))
+        with open(KEY_FILE, "wt") as f:
+            f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8"))
+
+    def checkCertPath(self):
+        exist = False
+        if (path.exists("certificate/cacert.pem") and path.exists("certificate/private.pem")):
+            exist = True
+        return exist
+
+
 def handler(signum, frame):
     pass
 
@@ -245,6 +278,9 @@ def main():
         signal.signal(signal.SIGQUIT, handler)
 
         if (args.ssl):
+            cert = Certificate()
+            if ((cert.checkCertPath() == False) or cert.checkCertificateExpiration()):
+                cert.genCertificate()
             server.socket = ssl.wrap_socket (server.socket, certfile='certificate/cacert.pem', keyfile='certificate/private.pem', server_side=True)
 
         server.handle_request()
