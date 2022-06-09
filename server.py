25f60fb28f8c5e0c1d069c323f2262f091c228c4
3v4Si0N <hector.de.armas.hda@gmail.com>
2022-06-09 13:21:10 +0200

    Fixed bug download functionality

--- server.py
+++ server.py
@@ -27,12 +27,12 @@ class myHandler(BaseHTTPRequestHandler):
         pwd = self.getPwd(json_response)
 
         if (self.isDownloadFunctCalled(json_response)):
-            filename, content, output = self.parseDownload(json_response)
+            filename, content = self.parseDownload(json_response)
             try:
                 with open(filename, mode='wb') as file: # b is importante -> binary
                     content = base64.b64decode(content)
                     file.write(content)
-                    print(Color.F_Green + output + Color.reset)
+                    print(Color.F_Green + result + Color.reset)
             except:
                 print (Color.F_Red + "\r\n[!] Error: Writing file!" + Color.reset)
         else:
@@ -81,11 +81,9 @@ class myHandler(BaseHTTPRequestHandler):
 
     def parseDownload(self, json_result):
         downloaded_file_path = ""
-        output = ""
         file_content = ""
 
         try:
-            output = json_result["result"]
             downloaded_file_path = json_result["pathDst"]
             file_content = json_result["file"]
         except KeyError:
